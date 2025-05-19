0ae177d8d25afa2b1af9af9baa2e0c891fb0d78f
test <darkscout900@gmail.com>
2025-05-19 13:41:52 +0300

    kek

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class Test {
     @org.junit.jupiter.api.Test
     public void test() {
-        open("C:\\TestSite.html");
+        //open("C:\\TestSite.html");
     }
 }
