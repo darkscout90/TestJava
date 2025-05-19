23d58ee23db954af542b89309ca569d357283d7d
test <darkscout900@gmail.com>
2025-05-19 17:18:29 +0300

    edit

--- test3/build.gradle.kts
+++ test3/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation ("com.codeborne:selenide:7.9.2")
+    implementation ("org.slf4j:slf4j-simple:1.7.36")
 }
 
 tasks.test {
--- test3/src/test/java/test.java
+++ test3/src/test/java/test.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Configuration;
+
+import java.net.URL;
+
 public class test {
+    @Test
+    public void test() {
+        Configuration.holdBrowserOpen = true;
+        URL url = ClassLoader.getSystemClassLoader().getResource("TestSite.html");
+        Selenide.open(url.toString());
+        System.out.println("All ok");
+    }
 }
